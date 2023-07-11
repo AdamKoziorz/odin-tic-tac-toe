94a16d3e6319f37361b55fbed154852a7cfc223e
AdamKoziorz <adam-koziorz@outlook.com>
2023-07-11 02:47:55 -0400

    Implement Functionality and Basic View
    The game is now functional and the requirements of the project have been met. However, some additional functionality as well as view improvements can and likely will be made in the future.

---
+++ script.js
@@ -0,0 +1,114 @@
+// "Player" factory for Tic Tac Toe
+const Player = (icon) => {
+    this.icon = icon;
+
+    const getIcon = () => {
+        return icon;
+    }
+
+    return { getIcon };
+};
+
+
+
+// "Board" module for Tic Tac Toe
+// Mapping of the view to indices
+// 0 1 2
+// 3 4 5
+// 6 7 8
+const gameBoard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const setCell = (type, index) => {
+        board[index] = type;
+    }
+
+    const getCell = (index) => {
+        return board[index];
+    }
+
+    return {setCell, getCell};
+})();
+
+
+
+// "Game Controller" module for Tic Tac Toe
+// This should be responsible for communicating with the game logic handling cell clicks in accord
+const gameController = (() => {
+    const playerOne = Player("X");
+    const playerTwo = Player("O");
+    let round = 1;
+    let currentPlayer = playerOne;
+    let isOver = false;
+
+    const checkWin = (newIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+          ];
+      
+          return winConditions
+            .filter((combination) => combination.includes(newIndex))
+            .some((possibleCombination) =>
+              possibleCombination.every(
+                (index) => gameBoard.getCell(index) === currentPlayer.getIcon()
+              )
+            );
+    }
+
+    const playRound = (index) => {
+        gameBoard.setCell(currentPlayer.getIcon(), index);
+        if (checkWin(index)) {
+            displayController.updateDialog(`Player ${currentPlayer.getIcon()} won!`);
+            isOver = true;
+        } else if (round === 9) {
+            displayController.updateDialog("Draw!");
+        } else {
+            round++;
+            currentPlayer = round % 2 === 0 ? playerTwo : playerOne;
+            displayController.updateDialog(`Player ${currentPlayer.getIcon()}'s Turn`);
+        }
+    }
+
+    const getIsOver = () => {
+        return isOver;
+    }
+
+    return {checkWin, playRound, getIsOver};
+})();
+
+
+
+// "Display Controller" module for Tic Tac Toe
+// This should be responsible for reacting to the cell clicks and changing the view,
+// including the dialog span
+const displayController = (() => {
+    const cells = document.querySelectorAll(".game-cell");
+    const dialog = document.getElementById("dialog");
+
+    cells.forEach((cell =>
+        cell.addEventListener("click", (e) => {
+            if ((!gameController.getIsOver()) && e.target.textContent === "") {
+                gameController.playRound(parseInt(e.target.dataset.index));
+                updateGrid();
+            }
+    })));
+
+    const updateGrid = () => {
+        for (let i = 0; i < cells.length; i++) {
+            cells[i].textContent = gameBoard.getCell(i);
+        }
+    }
+
+    const updateDialog = (message) => {
+        dialog.textContent = message;
+    }
+
+    return {updateDialog};
+})();
