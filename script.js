06920a860f169064f37face210127ebdb7f33f78
AdamKoziorz <adam-koziorz@outlook.com>
2023-07-11 22:40:59 -0400

    Add additional functionality and view
    Just for fun, I added the ability to reset as well as a scoreboard. I could possibly improve the view in the future since I ended up hardcoding the pixels.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // "Player" factory for Tic Tac Toe
 const Player = (icon) => {
     this.icon = icon;
+    let wins = 0;
 
     const getIcon = () => {
         return icon;
     }
 
-    return { getIcon };
+    const getWins = () => {
+        return wins;
+    }
+
+    const incrementWins = () => {
+        wins++;
+    }
+
+    return { getIcon, getWins, incrementWins };
 };
 
 
@@ -27,7 +36,13 @@ const gameBoard = (() => {
         return board[index];
     }
 
-    return {setCell, getCell};
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
+
+    return {setCell, getCell, resetBoard};
 })();
 
 
@@ -35,10 +50,10 @@ const gameBoard = (() => {
 // "Game Model" module for Tic Tac Toe
 // This should be responsible for implementing the game logic
 const gameModel = (() => {
-    const playerOne = Player("X");
-    const playerTwo = Player("O");
-    let round = 1;
-    let currentPlayer = playerOne;
+    const _playerOne = Player("X");
+    const _playerTwo = Player("O");
+    let _round = 1;
+    let _currentPlayer = _playerOne;
     let isOver = false;
 
     const checkWin = (newIndex) => {
@@ -57,31 +72,42 @@ const gameModel = (() => {
             .filter((combination) => combination.includes(newIndex))
             .some((possibleCombination) =>
               possibleCombination.every(
-                (index) => gameBoard.getCell(index) === currentPlayer.getIcon()
+                (index) => gameBoard.getCell(index) === _currentPlayer.getIcon()
               )
             );
     }
 
     const playRound = (index) => {
-        gameBoard.setCell(currentPlayer.getIcon(), index);
+        gameBoard.setCell(_currentPlayer.getIcon(), index);
         if (checkWin(index)) {
-            displayController.updateDialog(`Player ${currentPlayer.getIcon()} won!`);
+            displayController.updateDialog(`Player ${_currentPlayer.getIcon()} won!`);
+            _currentPlayer.incrementWins();
+            displayController.updateScore(_currentPlayer.getWins(), _currentPlayer.getIcon());
             isOver = true;
-        } else if (round === 9) {
+        } else if (_round === 9) {
             displayController.updateDialog("Draw!");
             isOver = true;
         } else {
-            round++;
-            currentPlayer = round % 2 === 0 ? playerTwo : playerOne;
-            displayController.updateDialog(`Player ${currentPlayer.getIcon()}'s Turn`);
+            _round++;
+            _currentPlayer = _round % 2 === 0 ? _playerTwo : _playerOne;
+            displayController.updateDialog(`Player ${_currentPlayer.getIcon()}'s Turn`);
         }
+    }
+
+    const resetGame = () => {
+        isOver = false;
+        _round = 1;
+        _currentPlayer = _playerOne;
+        gameBoard.resetBoard();
+        displayController.updateGrid();
+        displayController.updateDialog(`Player ${_currentPlayer.getIcon()}'s Turn`);
     }
 
     const getIsOver = () => {
         return isOver;
     }
 
-    return {checkWin, playRound, getIsOver};
+    return {checkWin, playRound, resetGame, getIsOver};
 })();
 
 
@@ -89,10 +115,13 @@ const gameModel = (() => {
 // "Display Controller" module for Tic Tac Toe
 // This should be responsible for handling user interaction and output
 const displayController = (() => {
-    const cells = document.querySelectorAll(".game-cell");
-    const dialog = document.getElementById("dialog");
+    const _cells = document.querySelectorAll(".game-cell");
+    const _dialog = document.getElementById("dialog");
+    const _resetBtn = document.getElementById("restart-btn");
+    const _xScore = document.getElementById("x-score");
+    const _oScore = document.getElementById("o-score");
 
-    cells.forEach((cell =>
+    _cells.forEach((cell =>
         cell.addEventListener("click", (e) => {
             if ((!gameModel.getIsOver()) && e.target.textContent === "") {
                 gameModel.playRound(parseInt(e.target.dataset.index));
@@ -100,15 +129,25 @@ const displayController = (() => {
             }
     })));
 
+    _resetBtn.addEventListener("click", gameModel.resetGame);
+
     const updateGrid = () => {
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].textContent = gameBoard.getCell(i);
+        for (let i = 0; i < _cells.length; i++) {
+            _cells[i].textContent = gameBoard.getCell(i);
         }
     }
 
     const updateDialog = (message) => {
-        dialog.textContent = message;
+        _dialog.textContent = message;
     }
 
-    return {updateDialog};
+    const updateScore = (score, icon) => {
+        if (icon == "X") {
+            _xScore.textContent = score;
+        } else if (icon == "O") {
+            _oScore.textContent = score;
+        }
+    }
+
+    return {updateGrid, updateDialog, updateScore};
 })();
